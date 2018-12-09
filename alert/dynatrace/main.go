e3fe6b07ad81abc6bc45e1eafb746707c0e8228e
Marc Arndt <marc@marcarndt.com>
2018-12-09 18:15:03 +0200

    adding main method

--- alert/dynatrace/main.go
+++ alert/dynatrace/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/weAutomateEverything/serverless-alerting/alert/lambda/client"
 	client2 "github.com/weAutomateEverything/serverless-alerting/alert/text/client"
 	"github.com/weAutomateEverything/serverless-alerting/common"
 )
 
 func main(){
-
+	lambda.Start(Handle)
 }
 
 func Handle(request events.APIGatewayProxyRequest)(response events.APIGatewayProxyResponse, err error ){
